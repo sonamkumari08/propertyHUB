9778bb9f95f615c09b12466af7f7c689382da5ac
sonam <sonamkumarikvs101@gmail.com>
2024-07-06 20:16:18 +0530

    vercel.json

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ const sessionOptions = {
 };
 
 
- app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
+// app.get("/", (req, res) => {
+ // res.send("Hi, I am root");
+//});
 
 
 const store = MongoStore.create({
