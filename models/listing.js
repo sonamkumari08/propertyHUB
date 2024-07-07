27c300fc7a979d0a27996a557a578f52f902ffeb
sonam kumari <169637442+sonamkumari08@users.noreply.github.com>
2024-07-07 14:08:53 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -15,7 +15,7 @@ const listingSchema = new Schema({
       "https://images.unsplash.com/photo-1533619239233-6280475a633a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNreSUyMHZhY2F0aW9ufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
     set: (v) =>
       v === ""
-        ?   "https://images.unsplash.com/photo-1587381420270-3e1a5b9e6904?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxvZGdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+        ?   "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
         : v,
   },
   price: Number,
